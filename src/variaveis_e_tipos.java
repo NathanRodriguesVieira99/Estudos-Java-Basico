f6e5f745cf3d3419ce8c8012896dc4c9f9450719
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-25 15:51:19 -0300

    feat: add variaveis e tipos

---
+++ src/variaveis_e_tipos.java
@@ -0,0 +1,52 @@
+public class variaveis_e_tipos {
+    public static void main(String[] args) {
+
+        // Tipos primitivos
+        // declarar uma variavel -> tipo nome = valor
+
+        // 1. int
+        int numeroInt = 42;
+        System.out.println("int: " + numeroInt);
+        System.out.println("===========================");
+
+        // 2. long
+        long numeroLong = 1234567890123L; // 'L' no final para indicar que é um long
+        System.out.println("long: " + numeroLong);
+        System.out.println("===========================");
+
+        // short
+        short numeroShort = 12345;
+        System.out.println("short: " + numeroShort);
+        System.out.println("===========================");
+
+        // byte
+        byte numeroByte = 127;
+        System.out.println("byte: " + numeroByte);
+        System.out.println("===========================");
+
+        // float
+        float numeroFloat = 3.14f; // 'f' no final para indicar que é um float
+        System.out.println("float: " + numeroFloat);
+        System.out.println("===========================");
+
+        // double
+        double numeroDouble = 3.141592653589793;
+        System.out.println("double: " + numeroDouble);
+        System.out.println("===========================");
+
+        // char
+        char caractere = 'A';
+        System.out.println("char: " + caractere);
+        System.out.println("===========================");
+
+        // boolean
+        boolean booleano = true;
+        System.out.println("boolean: " + booleano);
+        System.out.println("===========================");
+
+        // String
+        String texto = "Olá, mundo!";
+        System.out.println("String: " + texto);
+        System.out.println("===========================");
+    }
+}

437613ad9785b01878f01be4f5c0edf5f3648c9d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-30 11:42:19 -0300

    feat: add read txt w java

--- src/EscreverEmTxt.java
+++ src/EscreverEmTxt.java
@@ -1,14 +1,34 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class EscreverEmTxt {
     public static void main(String[] args) throws Exception {
         // escrever no txt
-        String caminhoArquivo = "arquivo.txt";
+        String caminhoArquivo = "C:\\Projetos VsCode\\Estudos java basico\\arquivo.txt";
+
         BufferedWriter escritor = new BufferedWriter(new FileWriter(caminhoArquivo));
 
-        escritor.write("Olá Mundo da Silva \n tudo bem?");
-
+        escritor.write("Olá Mundo da Silva \n tudo bem? kkkkkk");
         escritor.close();
+
+        // ler o txt
+        BufferedReader leitor = null;
+
+        try {
+            FileReader leitorDeArquivos = new FileReader(caminhoArquivo);
+            leitor = new BufferedReader(leitorDeArquivos);
+
+            String linha = leitor.readLine();
+            while (linha != null) {
+                System.out.println(linha);
+                linha = leitor.readLine();
+            }
+
+        } catch (Exception erro) {
+            System.out.println("Erro ao ler");
+            System.out.println(erro.getMessage());
+        }
     }
 }
